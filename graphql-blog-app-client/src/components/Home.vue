0de3d63b29df745f37390f36aee7ef7b646cf357
Calin Georgescu <calinrulz@gmail.com>
2018-04-22 13:11:32 +0300

    added Axios and clg the users on Home page

--- graphql-blog-app-client/src/components/Home.vue
+++ graphql-blog-app-client/src/components/Home.vue
@@ -12,12 +12,15 @@
               {{ post.title }}
             </router-link>
         </h3>
+
+        <button class="button is-warning" @click="getUsers">Get Users</button>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import axios from 'axios';
 import { ALL_POSTS_QUERY } from '@/graphql';
 
 export default {
@@ -27,6 +30,21 @@ export default {
       allPosts: [],
     };
   },
+  // With Axios
+  methods: {
+    async getUsers() {
+      try {
+        const requestPosts = await axios.post('http://localhost:3333/graphql', {
+          query: ALL_POSTS_QUERY,
+        });
+        const resolvePosts = requestPosts.data.data.allPosts;
+        console.log(resolvePosts);
+      } catch (error) {
+        console.log('error: ', error);
+      }
+    },
+  },
+  // Without Axios
   apollo: {
     // fetch all posts
     allPosts: {
